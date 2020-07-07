acfa7e04bf11b97d4e2d10726373abfba40dc3a3
Florian <florian.zebidi@icaretech.fr>
2020-07-07 23:44:41 +0200

    add generation of toc for markdown content

--- src/template.rs
+++ src/template.rs
@@ -36,6 +36,128 @@ struct LoginContent<'a> {
     title: &'a str,
 }
 
+use std::str::FromStr;
+
+pub struct Heading {
+    pub depth: usize,
+    pub title: String,
+}
+
+impl FromStr for Heading {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let trimmed = s.trim_right();
+
+        if trimmed.starts_with("#") {
+            let mut depth = 0usize;
+            let title = trimmed
+                .chars()
+                .skip_while(|c| {
+                    if *c == '#' {
+                        depth += 1;
+                        true
+                    } else {
+                        false
+                    }
+                }).collect::<String>()
+                .trim_left()
+                .to_owned();
+
+            Ok(Heading {
+                depth: depth - 1,
+                title,
+            })
+        }
+        else {
+            Err(())
+        }
+    }
+}
+
+impl Heading {
+    pub fn format(&self) -> Option<String> {
+        //let depth : usize;
+        //if self.depth > 2 { depth = 2; }
+        //else { depth = self.depth; }
+
+        Some(format!(
+            "{} {} {}",
+            " ".repeat(2)
+                .repeat(self.depth),
+            "*",
+            format!("[{}](#user-content-{})", &self.title, &self.title.replace(" ", "-").to_lowercase() )
+        ))
+    }
+}
+
+pub struct ContentM {
+    pub line: String,
+}
+
+impl FromStr for ContentM {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let trimmed = s.trim_right();
+
+        if trimmed.starts_with("#") {
+            let mut depth = 0usize;
+            let line = trimmed
+                .chars()
+                .skip_while(|c| {
+                    if *c == '#' {
+                        depth += 1;
+                        true
+                    } else {
+                        false
+                    }
+                }).collect::<String>()
+                .trim_left()
+                .to_owned();
+
+            Ok(ContentM {
+                line : format!("{} <a id=\"user-content-{}\"></a>", s, line.replace(" ", "-").to_lowercase() ) ,
+            })
+        }
+        else {
+            Ok(ContentM {
+                line : s.to_owned(),
+            })
+        }
+    }
+}
+
+pub fn add_markdown_toc( content : &str ) -> Result<String, ApiError> {
+    let mut result = String::new();
+
+    content
+        .lines()
+        .map(Heading::from_str)
+        .filter_map(Result::ok)
+        .filter_map(|h| h.format() )
+        .for_each(|l| {
+            result.push_str( &l );
+            result.push('\n');
+        });
+
+    result.push('\n');
+
+    content
+        .lines()
+        .map(ContentM::from_str)
+        .filter_map(Result::ok)
+        .for_each(|l| {
+            //println!("{}", l.line);
+            result.push_str( &l.line );
+            result.push('\n');
+        });
+    
+    println!("{}", result);
+
+    Ok( result )
+}
+
 pub fn post_template( id : &str ) -> Result<String, ApiError> {
 
     let tpl = Template::from_file("./src/templates/post_page.html") ? ;
@@ -44,7 +166,7 @@ pub fn post_template( id : &str ) -> Result<String, ApiError> {
     let posts_content =
         PostContent {
             title : &post.title,
-            body : &post.body,
+            body : &add_markdown_toc( &post.body ) ?,
             date : format!("{}", &post.created_at),
         };
 
